a45cbfd24c9000f620d8aaca766ea0b17e2a2dfa
Couch Janus <couchjanus@gmail.com>
2020-01-24 21:01:08 +0200

    add dashboard

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -3,27 +3,19 @@
 require_once MODELS.'/User.php';
 require_once CORE.'/Session.php';
 
-class DashboardController extends Controller
-{
-    private $user;
-    
+class DashboardController extends Admin{
+
     public function __construct()
     {
         parent::__construct();
-        Session::init();
-        $userId = Session::get('userId');
-        if ($userId) {
-            $this->user = User::getByPrimaryKey($userId);
-        } else {
-            $this->user = null;
-        }
+        
     }
     public function index()
     {
-        if (!$this->user) {
+        if (!$this->isAdmin()) {
             Helper::redirect('/login');
         }
-        if ($this->user->role_id == 1) {
+        else if ($this->isAdmin() == true) {
             $title = 'Admin Profile';
             $this->view->render('admin/index', compact('user',  'title'), 'admin');
         } else {
